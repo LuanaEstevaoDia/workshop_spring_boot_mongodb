a9cce6d3047b819b079fc2467b6b569a71bc9a4e
LuanaEstevaoDia <luanafreitas.ti@gmail.com>
2024-10-05 14:01:38 -0300

    Obtendo um post por id

---
+++ src/main/java/com/nelioalves/worhshomongo/sevices/PostService.java
@@ -0,0 +1,25 @@
+package com.nelioalves.worhshomongo.sevices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.worhshomongo.domain.Post;
+import com.nelioalves.worhshomongo.repository.PostRepository;
+import com.nelioalves.worhshomongo.sevices.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+
+	
+	public Post findById(String id) {
+	    return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	  
+	}
+	
+		
+	}
+
