1f063493803a3a73660658b20e7f0b9b28c44d71
LuanaEstevaoDia <luanafreitas.ti@gmail.com>
2024-10-05 13:19:40 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/nelioalves/worhshomongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/worhshomongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nelioalves.worhshomongo.domain.Post;
 import com.nelioalves.worhshomongo.domain.User;
 import com.nelioalves.worhshomongo.dto.UserDTO;
 import com.nelioalves.worhshomongo.sevices.UserService;
@@ -68,6 +69,14 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+		
+	}
+	
 	
 	
 		
